9bfffe615536133c2f376cbbd7d00578f490879d
Tommy Lloyd <tommylloyd@northwesternmutual.com>
2018-06-14 11:47:34 -0500

    Added a path to save incoming JSON object to mongo database

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const debug = require('debug')('api:app');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/clientData";
 const health = require('./routes/health.route');
 const clients = require('./routes/clients.route');
 const places = require('./routes/places.route');
@@ -53,5 +55,21 @@ app.use((err, req, res, next) => {
     });
     next(err);
 });
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("clientData");
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  });
 
 module.exports = app;
--- routes/incoming.route.js
+++ routes/incoming.route.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
+//accepts an JSON object and add in to e database
+/* ex. {
+	"email":"afr@northwesternmutual.com",
+	"busID":"msTroJP3a8TopV-dS62rBw",
+	"date":"2018-06-14",
+	"clientID":"client@gmail.com",
+	"nmRating": "2.5"
+} */
 
 router.post('/', (req, res) => {
-    const body = req.body.Body;
-    res.set('Content-Type', 'text/json')
-    res.send(`You sent: ${body} to Express`)
-  });
+    const body = req.body;
 
-  module.exports = router;
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("clientData");
+      var myobj = { 
+        email: body.email, 
+        busID: body.busID,
+        date: body.date,
+        clientID: body.clientID,
+        nmRating: body.nmRating
+      };
+      
+      dbo.collection("customers").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+      });
+  })
+});
+
+module.exports = router;
